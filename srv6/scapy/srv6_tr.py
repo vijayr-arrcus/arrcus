ecb7e99a97c421904ede1e07001838de4e6b6184
Vijay Rajamani <vijayr@arrcus.com>
2021-05-09 15:42:47 +0000

    show and sleep

--- srv6/scapy/srv6_tr.py
+++ srv6/scapy/srv6_tr.py
@@ -31,6 +31,8 @@ udp_dst_port=33434
 
 def send_packet(p, intf, t_count):
     i = 0
+    p.show()
+    time.sleep(5)
     while (i < t_count):
         sendp(p, iface=intf, count=1000)
         i = i+1000
