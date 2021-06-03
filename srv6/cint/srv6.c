71e75f19bbc795e7234b45edfa0b8aec79a3f5ff
Vijay Anand r <vjanandr85@gmail.com>
2021-06-03 12:39:09 +0000

    srv5 64

--- srv6/cint/srv6.c
+++ srv6/cint/srv6.c
@@ -29,8 +29,7 @@ int intf_ingress_rif_set(int unit, l3_ingress_intf * ingress_itf)
 }
 
 void
-l3_ingress_intf_init(
-    l3_ingress_intf * ingr_intf)
+l3_ingress_intf_init(l3_ingress_intf * ingr_intf)
 {
     ingr_intf->vrf = 0;
     ingr_intf->intf_id = 0;
@@ -38,12 +37,10 @@ l3_ingress_intf_init(
     ingr_intf->qos_map = 0;
 }
 
-bcm_ip6_t ip6_mask = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-bcm_ip6_t ip6_mask_96_loc = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x00};
-bcm_ip6_t ip6_mask_96_func = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xFF,0xFF,0x00,0x00};
-bcm_ip6_t ip6_dip_96 = {0,0,0,0,0,0,0,0,0x12,0x34,0,0,0xFF,0xFF,0xFF,0x13};
-bcm_ip6_t ip6_sip = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0xAA};
-bcm_ip6_t ip6_dip_next_endpoint = {0,0,0,0,0,0,0,0,0x12,0x34,0,0,0,0,0xFF,0x13};
+bcm_ip6_t ip6_mask = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x00,0x00,0x00};
+bcm_ip6_t ip6_mask_64_loc = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+bcm_ip6_t ip6_mask_64_func = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+bcm_ip6_t ip6_dip_64 = {0x20,0x01,0xab,0xcd,0xca,0xfe,0x20,0x00,0x81,0x01,0x00,0x00,0x00,0x00,0x00,0x00};
 bcm_tunnel_terminator_t tunnel_term_set;
 l3_ingress_intf ingress_rif;
 int rv;
@@ -54,13 +51,13 @@ int unit=0;
 fec += 0x500;
 
 /*
-* 1. Create 96 locator tunnel VRF is 10
+* 1. Create 64 locator tunnel VRF is 10
 */
 bcm_tunnel_terminator_t_init(&tunnel_term_set);
-tunnel_term_set.flags = BCM_TUNNEL_TERM_UP_TO_96_LOCATOR_SEGMENT_ID;
+tunnel_term_set.flags = BCM_TUNNEL_TERM_UP_TO_64_LOCATOR_SEGMENT_ID;
 tunnel_term_set.type = bcmTunnelTypeSR6;
-sal_memcpy(tunnel_term_set.dip6, ip6_dip_96, 16);
-sal_memcpy(tunnel_term_set.dip6_mask, ip6_mask_96_loc, 16);
+sal_memcpy(tunnel_term_set.dip6, ip6_dip_64, 16);
+sal_memcpy(tunnel_term_set.dip6_mask, ip6_mask_64_loc, 16);
 tunnel_term_set.vrf = 1;
 tunnel_term_set.ingress_qos_model.ingress_ttl = bcmQosIngressModelPipe;
 tunnel_term_set.ingress_qos_model.ingress_phb = bcmQosIngressModelPipe;
@@ -80,8 +77,8 @@ default_tunnel = tunnel_term_set.tunnel_id;
 bcm_tunnel_terminator_t_init(&tunnel_term_set);
 tunnel_term_set.type = bcmTunnelTypeCascadedFunct;
 tunnel_term_set.default_tunnel_id = default_tunnel;
-sal_memcpy(tunnel_term_set.dip6, ip6_dip_96, 16);
-sal_memcpy(tunnel_term_set.dip6_mask, ip6_mask_96_func, 16);
+sal_memcpy(tunnel_term_set.dip6, ip6_dip_64, 16);
+sal_memcpy(tunnel_term_set.dip6_mask, ip6_mask_64_func, 16);
 tunnel_term_set.vrf = 1;
 tunnel_term_set.ingress_qos_model.ingress_ttl = bcmQosIngressModelPipe;
 tunnel_term_set.ingress_qos_model.ingress_phb = bcmQosIngressModelPipe;
