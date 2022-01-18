55c3c7ab5519166aab4e9b7d6c821728b8df5fa9
Vijay Rajamani <vijayr@arrcus.com>
2022-01-18 11:31:55 +0000

    more route

--- td4/cints/td4_route.c
+++ td4/cints/td4_route.c
@@ -17,6 +17,7 @@
  */
 #include <stdlib.h>
 cint_reset();
+int unit = 0;
 bcm_l3_egress_t egr_obj;
 bcm_if_t egr_obj_if;
 int rv = BCM_E_NONE;
@@ -32,17 +33,18 @@ route_t.l3a_ip_mask = 0xffffff00;
 route_t.l3a_intf = egr_obj_if;
 route_t.l3a_vrf = 0;
 route_t.l3a_opaque_ctrl_id = 6;
+route_t.l3a_lookup_class = 6;
 rv = bcm_l3_route_add(unit, &route_t);
 if (BCM_FAILURE(rv)) {
     printf(“Error in bcm_l3_route_add: %s\n”, bcm_errmsg(rv));
 }
 bcm_l3_host_t host_t;
 bcm_l3_host_t_init(&host_t);
-host_t.l3a_subnet = 0x0A010101;
-host_t.l3a_ip_mask = 0xffffffff;
+host_t.l3a_ip_addr = 0xffffffff;
 host_t.l3a_intf = egr_obj_if;
 host_t.l3a_vrf = 0;
 host_t.l3a_opaque_ctrl_id = 6;
+host_t.l3a_lookup_class = 6;
 rv = bcm_l3_host_add(unit, &host_t);
 if (BCM_FAILURE(rv)) {
     printf(“Error in bcm_l3_route_add: %s\n”, bcm_errmsg(rv));
