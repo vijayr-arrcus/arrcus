7aef102398b36c0ab1691a35b5ea913eafa96cb9
Vijay Rajamani <vijayr@arrcus.com>
2022-03-25 12:15:20 +0000

    scale debugging

--- td4/cints/knet_pmd.c
+++ td4/cints/knet_pmd.c
@@ -47,15 +47,23 @@ filter.match_flags = BCM_PKTIO_FILTER_M_INGPORT;
 filter.m_ingport = 82;
 print bcm_pktio_filter_create(unit, &filter);
 
+cint_reset();
+bcm_pktio_netif_t netif;
+bcm_pktio_filter_t filter;
+int unit = 0;
 bcm_pktio_filter_t_init(&filter);
 filter.type = BCM_PKTIO_FILTER_T_RX_PKT;
-filter.priority = 10;
+filter.priority = 1;
 filter.dest_type = BCM_PKTIO_DEST_T_BCM_RX_API;
-sal_memcpy(filter.desc, "Filter netdev-swp18-100", 20);
-filter.dest_id = netif.id;
+filter.dest_type = BCM_PKTIO_DEST_T_NETIF;
+sal_memcpy(filter.desc, "Filter bcm", 20);
+filter.dest_id = 1;
 filter.match_flags = BCM_PKTIO_FILTER_M_INGPORT;
-filter.m_ingport = 82;
+filter.m_ingport = 81;
 print bcm_pktio_filter_create(unit, &filter);
+
+pw stop
+pw start report +raw +pmd +decode
 
 
 bcm_pktio_filter_destroy(0, )
