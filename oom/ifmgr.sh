310c4da821577baea97e26c4c7580a4c29610aa5
Vijay Rajamani <vijayr@arrcus.com>
2023-07-12 23:14:50 +0000

    asan logs

--- oom/ifmgr.sh
+++ oom/ifmgr.sh
@@ -2,5 +2,5 @@
 rm /tmp/asan.log
 touch /tmp/asan.log
 chmod 777 /tmp/asan.log
-export ASAN_OPTIONS="log_path=/tmp/asan.log:verbosity=1:abort_on_error=1:disable_coredump=0:unmap_shadow_on_exit=1"
-ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer /usr/lib/arcos/ifmgr.bin
+export ASAN_OPTIONS="log_path=/tmp/asan.log:verbosity=1:abort_on_error=0:disable_coredump=0:unmap_shadow_on_exit=1"
+ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer /usr/lib/arcos/dpal.bin
