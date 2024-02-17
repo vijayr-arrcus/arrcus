b20dec151973b3adfffd8f06df51441bfa4af175
Vijay Rajamani <vijayr@arrcus.com>
2024-02-17 09:27:11 +0000

    track all mem

--- track_all_mem.sh
+++ track_all_mem.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #ST sleep timer.
-ST=300
+ST=1800
 for ((i=1;1;i++))
 do
-echo "${i}. ***********************************************";
-date; top -n 1 -b o %MEM;
-echo "${i}. *********************************************** END";
+echo "${i}. ***********************************************" >> /tmp/mem.log;
+date >> /tmp/mem.log;
+echo -en "\n" >> /tmp/mem.log;
+free >> /tmp/mem.log;
+echo -en "\n" >> /tmp/mem.log;
+top -n 1 -b o %MEM >> /tmp/mem.log;
 #for ((j=0;$j<$ST;j++)) do sleep 1; echo -en "$(date) \t - wait $(($ST - $j)) \r"; done
+echo "${i}. *********************************************** END" >> /tmp/mem.log;
 sleep $ST
-printf "\n\n\n"
+echo -en "\n\n\n" >> /tmp/mem.log;
 done
 
