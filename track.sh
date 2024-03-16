ed44867b1c4839d5afcd7c0599df3d76877135ae
Vijay Rajamani <vijayr@arrcus.com>
2024-03-16 05:19:55 +0000

    mem leaks

--- track.sh
+++ track.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 #ST sleep timer.
-proc_list = (
-"dpal.bin"
-"ifmgr.bin"
-)
-ST=300
+ST=1800
 PROC=$1
 mkdir -p /tmp/pmap/$PROC
 for ((i=1;1;i++))
 do
-echo "${i}. ***********************************************";
-date; top -n 1 -b -p $(pidof $PROC);
-#kill -10 $(pidof $PROC)
+echo "${i}. ***********************************************" >> /tmp/mem.log
+date >> /tmp/mem.log
+top -n 1 -b o %MEM >> /tmp/mem.log;
+#kill -12 $(pidof $PROC)
 date >> /tmp/pmap/$PROC/${i}_pmap.log
 pmap -XX $(pidof $PROC) >> /tmp/pmap/$PROC/${i}_pmap.log
-echo "${i}. *********************************************** END";
+echo "${i}. *********************************************** END" >> /tmp/mem.log
 #for ((j=0;$j<$ST;j++)) do sleep 1; echo -en "$(date) \t - wait $(($ST - $j)) \r"; done
 sleep $ST
 printf "\n\n\n"
