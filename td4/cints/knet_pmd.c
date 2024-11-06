76800bdaea4a96bc97e1990b01efe214e60ff7c5
Vijay Rajamani <vijayr@arrcus.com>
2024-11-06 10:09:36 +0000

    prefix

--- td4/cints/knet_pmd.c
+++ td4/cints/knet_pmd.c
@@ -61,14 +61,14 @@ filter.dest_type = BCM_PKTIO_DEST_T_NETIF;
 sal_memcpy(filter.desc, "Filter bcm", 20);
 filter.dest_id = 1;
 filter.match_flags = BCM_PKTIO_FILTER_M_INGPORT;
-filter.m_ingport = 43;
+filter.m_ingport = 100;
 print bcm_pktio_filter_create(unit, &filter);
 
 pw stop
 pw start report +raw +pmd +decode
 
 
-bcm_pktio_filter_destroy(0, )
+bcm_pktio_filter_destroy(0, filter.id);
 bcm_pktio_netif_destroy(0, )
 
 cint_reset();
